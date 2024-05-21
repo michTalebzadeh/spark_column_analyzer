6f37e198b08e851ca00ee7e03645be96e146cdb6
git@github.com/michTalebzadeh <mich.talebzadeh@gmail.com>
2024-05-21 11:15:22 +0100

    dated 21st May 2024

--- setup.py
+++ setup.py
@@ -1,22 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='spark-column-analyzer',
-    version='0.2.1',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    author='Mich Talebzadeh',
-    author_email='mich.talebzadeh@gmail.com',
-    description='A package for analyzing PySpark DataFrame columns',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/michTalebzadeh/spark_column_analyzer',
+    name='spark_column_analyzer',
+    version='0.2.2',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[
         'pyspark',
     ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
+    include_package_data=True,
+    zip_safe=False,
 )
